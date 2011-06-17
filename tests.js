496828b28459ec5c03483b6e7b5abcc54dcef32a
Mariano Iglesias <mariano@cricava.com>
2011-06-17 10:49:51 -0300

    Updating node-db

--- tests.js
+++ tests.js
@@ -1,10 +1,15 @@
 /* Escape & Query building tests */
 
-var mysql = require("./db-mysql");
+var client = require("./db-oracle");
 var tests = require("./lib/node-db/tests.js").get(function() {
-    return new mysql.Database();
-});
+    return new client.Database();
+}, '"');
 
 for(var test in tests) {
     exports[test] = tests[test];
 }
+
+delete exports["Client"]["escape()"];
+delete exports["Query"]["select markers"];
+delete exports["Query"]["insert markers"];
+delete exports["Query"]["select values"];
